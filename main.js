2dba6cacc24b4f5d9ff06c3f5f2925b78cae882d
JoshuaEA12 <joshuabenjasolanojimenes@gmail.com>
2026-03-26 15:23:43 -0600

    java complete

---
+++ main.js
@@ -0,0 +1,9 @@
+function changebackgroundcolorblack(){
+    document.body.style.backgroundColor="black";
+}
+function changebackgroundcolorgreen(){
+    document.body.style.backgroundColor="green";
+}
+function changebackgroundcolorpink(){
+    document.body.style.backgroundColor="pink";
+}
